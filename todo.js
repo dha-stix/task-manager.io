851502c97f416c7f92281b571b0719cffb8b025f
Qudusayo <zeekimono@gmail.com>
2020-07-18 11:48:42 +0100

    Refactored, SimplifiedJS for beginners and added clear control to HTML

--- todo.js
+++ todo.js
@@ -1,13 +1,15 @@
 //SELECTORS
-let submitBtn = document.querySelector(".add");
-let clearBtn = document.querySelector(".clear");
+const submitBtn = document.querySelector(".add");
+const clearBtn = document.querySelector(".clear");
+const lists = document.querySelector(".todo-list");
+const input = document.querySelector("input");
+let todos;
 
 //FUNCTIONS
-
 const addTodo = (e) => {
   e.preventDefault();
-  let lists = document.querySelector(".todo-list");
-  let inputValue = document.querySelector("input").value;
+
+  let inputValue = input.value;
 
   //Check if a todo inputted, throws back an error message if it's not
   if (!inputValue) {
@@ -18,44 +20,10 @@ const addTodo = (e) => {
     list.textContent = inputValue;
 
     //Add to local storage
-    saveLocalTodos(document.querySelector("input").value);
-    //Creates a span in order to wrap the buttons in a span for equal spacing (flex)
-    let span = document.createElement("span");
-    span.style.width = "30%";
-    span.style.display = "flex";
-    span.style.justifyContent = "space-between";
-
-    //Adds the delete button to each todo-list created
-    let deleteBtn = document.createElement("i");
-    deleteBtn.className = "fas fa-trash-alt";
-    deleteBtn.classList.add("delBtn");
-
-    //Adds edit button to each todo-list created
-    let editBtn = document.createElement("i");
-    editBtn.className = "far fa-edit";
-    editBtn.classList.add("editBtn");
-
-    //An event that deletes each input by click the delete button
-    deleteBtn.addEventListener("click", () => {
-      lists.removeChild(list);
-      removeTodo(todo);
-    });
-
-    //An event that edits each input by the clicking the edit button
-    editBtn.addEventListener("click", () => {
-      document.querySelector("input").value = list.textContent;
-      document.querySelector("input").focus();
-      lists.removeChild(list);
-    });
-
-    //Appends the delete and edit button to the span, the span to the list and each list to the ul
-    span.appendChild(deleteBtn);
-    span.appendChild(editBtn);
-    list.appendChild(span);
-    lists.appendChild(list);
+    saveLocalTodos(input.value);
 
     //Clears the input field after a list has been added
-    document.querySelector("input").value = "";
+    input.value = "";
   }
 };
 
@@ -63,29 +31,30 @@ document.querySelector(".startBtn").addEventListener("click", function () {
   document.querySelector(".app").classList.add("show");
   document.querySelector(".intro-div").style.display = "none";
 });
+
 const clearTodo = (e) => {
   e.preventDefault();
-  let lists = document.querySelectorAll("li");
-  let parentList = document.querySelector(".todo-list");
-  for (let i = 0; i < lists.length; i++) {
-    parentList.removeChild(lists[i]);
-  }
+  
+  const todoList = document.querySelector(".todo-list");
+  todoList.innerHTML = "";
   localStorage.removeItem("todos");
 };
 
 function saveLocalTodos(todo) {
-  let todos;
   if (localStorage.getItem("todos") === null) {
     todos = [];
   } else {
     todos = JSON.parse(localStorage.getItem("todos"));
   }
-  todos.push(todo);
+  todos.unshift(todo);
   localStorage.setItem("todos", JSON.stringify(todos));
+
+  const todoList = document.querySelector(".todo-list");
+  todoList.innerHTML = "";
+  getTodos()
 }
 
 const getTodos = function () {
-  let todos;
   if (localStorage.getItem("todos") === null) {
     todos = [];
   } else {
@@ -121,11 +90,12 @@ const getTodos = function () {
     });
 
     //An event that edits each input by the clicking the edit button
-    editBtn.addEventListener("click", () => {
-      document.querySelector("input").value = list.textContent;
-      document.querySelector("input").focus();
-      lists.removeChild(list);
-    });
+    // editBtn Under Construction
+    // editBtn.addEventListener("click", () => {
+    //   input.value = list.textContent;
+    //   input.focus();
+    //   lists.removeChild(list);
+    // });
 
     //Appends the delete and edit button to the span, the span to the list and each list to the ul
     span.appendChild(deleteBtn);
@@ -133,10 +103,10 @@ const getTodos = function () {
     list.appendChild(span);
     lists.appendChild(list);
   });
+  showClearButton()
 };
 
 function removeTodo(todo) {
-  let todos;
   if (localStorage.getItem("todos") === null) {
     todos = [];
   } else {
@@ -144,6 +114,15 @@ function removeTodo(todo) {
   }
   todos.splice(todos.indexOf(todo), 1);
   localStorage.setItem("todos", JSON.stringify(todos));
+  showClearButton()
+}
+
+const showClearButton = ()  => {
+  if(todos.length < 1){
+    document.querySelector('#clearList').style.display = "none"
+  }else{
+    document.querySelector('#clearList').style.display = "block"
+  }
 }
 
 //EVENTS
